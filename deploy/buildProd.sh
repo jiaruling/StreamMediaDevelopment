183b603231b09389b11f38ee526506c56872c458
jiaruling@libii.com <jiaruling@libii.com>
2023-04-11 15:29:00 +0800

    🐞 fix(deploy): 部署shell脚本优化
    新增 stop、start、restart功能

--- deploy/buildProd.sh
+++ deploy/buildProd.sh
@@ -7,7 +7,7 @@ cd /application/StreamMediaDevelopment/scheduler
 env GOOS=linux GOARCH=amd64 go build -o ../bin/scheduler main.go
 
 cd /application/StreamMediaDevelopment/streamserver
-env GOOS=linux GOARCH=amd64 go build -o ../bin/streamserver main.go
+env GOOS=linux GOARCH=amd64 go build -o ../bin/stream main.go
 
 cd /application/StreamMediaDevelopment/web
 env GOOS=linux GOARCH=amd64 go build -o ../bin/web main.go
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,59 +1,81 @@
 #! /bin/bash
 
-# 拷贝资源
-echo "copy resource"
-cp -R ../template ../bin/
-cp ../conf.json ../bin/
-
-if [ ! -d ../bin/videos ]; then
-    mkdir ../bin/videos
+# 命令行参数
+mod=$1 # stop start restart
+if [ "$mod" != "stop" ] && [ "$mod" != "start" ] && [ "$mod" != "restart" ]; then
+    echo "arguments error: $mod"
+    exit 1
 fi
 
-cd ../bin
+# 拷贝资源
+if [ "$mod" == "start" ] || [ "$mod" == "restart" ]; then
+    echo "copy resource"
+    cp -R ../template ../bin/
+    cp ../conf.json ../bin/
+
+    if [ ! -d ../bin/videos ]; then
+        mkdir ../bin/videos
+    fi
+
+    cd ../bin
+fi
 
 # 删除之前的进程
-echo "kill api..."
-pid=$(netstat -ntlp | grep ./api | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
-if [ $pid -gt 0 ]; then
-    kill $pid
+if [ "$mod" == "stop" ] || [ "$mod" == "restart" ]; then
+    echo "kill api..."
+    pid=$(netstat -ntlp | grep ./api | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
+    if [ -n "$pid" ] && [ $pid -gt 0 ]; then
+        kill $pid
+    fi
+    echo "kill scheduler..."
+    pid=$(netstat -ntlp | grep ./scheduler | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
+    if [ -n "$pid" ] && [ $pid -gt 0 ]; then
+        kill $pid
+    fi
+    echo "kill stream..."
+    pid=$(netstat -ntlp | grep ./stream | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
+    if [ -n "$pid" ] && [ $pid -gt 0 ]; then
+        kill $pid
+    fi
+    echo "kill web..."
+    pid=$(netstat -ntlp | grep ./web | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
+    if [ -n "$pid" ] && [ $pid -gt 0 ]; then
+        kill $pid
+    fi
+    echo "kill finished"
 fi
-if [ ! -d ./api.log ]; then
-    rm -f ./api.log
-fi
-echo "restart api..."
-nohup ./api > api.log 2>&1 &
 
-echo "kill scheduler..."
-pid=$(netstat -ntlp | grep ./scheduler | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
-if [ $pid -gt 0 ]; then
-    kill $pid
-fi
-if [ ! -d ./scheduler.log ]; then
-    rm -f ./scheduler.log
-fi
-echo "restart scheduler..."
-nohup ./scheduler > scheduler.log 2>&1 &
+if [ "$mod" == "start" ] || [ "$mod" == "restart" ]; then
+    if [ ! -d ./api.log ]; then
+        rm -f ./api.log
+    fi
+    echo "start api..."
+    nohup ./api > api.log 2>&1 &
 
-echo "kill streamserver..."
-pid=$(netstat -ntlp | grep ./streamserver | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
-if [ $pid -gt 0 ]; then
-    kill $pid
-fi
-if [ ! -d ./streamserver.log ]; then
-    rm -f ./streamserver.log
-fi
-echo "restart streamserver..."
-nohup ./streamserver > streamserver.log 2>&1 &
+    if [ ! -d ./scheduler.log ]; then
+        rm -f ./scheduler.log
+    fi
+    echo "start scheduler..."
+    nohup ./scheduler > scheduler.log 2>&1 &
 
-echo "kill web..."
-pid=$(netstat -ntlp | grep ./web | awk '{print $7}' | awk 'match($0, /[0-9]+/) { print substr($0, RSTART, RLENGTH) }')
-if [ $pid -gt 0 ]; then
-    kill $pid
-fi
-if [ ! -d ./web.log ]; then
-    rm -f ./web.log
-fi
-echo "restart web..."
-nohup ./web > web.log 2>&1 &
+    if [ ! -d ./stream.log ]; then
+        rm -f ./stream.log
+    fi
+    echo "start stream..."
+    nohup ./stream > stream.log 2>&1 &
 
-echo "deploy finished"
+    if [ ! -d ./web.log ]; then
+        rm -f ./web.log
+    fi
+    echo "start web..."
+    nohup ./web > web.log 2>&1 &
+    echo "deploy finished"
+    sleep 1
+    echo "*************************************************************************************"
+    netstat -ntlp | grep -E "api|stream|scheduler|web"
+    echo "*************************************************************************************"
+fi
+
+echo "execute finished"
+
+
